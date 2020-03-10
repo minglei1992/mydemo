0203a51bf6c1ecff72161a8894beae423e16d12b
Administrator <minglei@123>
2020-03-10 17:18:42 +0800

    mydemo

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import lombok.Data;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
